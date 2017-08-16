a0650a8072c91b47d4a721893a7ba0dcaf13bbe0
Marcos001 <santosMsantos01@gmail.com>
2017-08-16 12:42:32 -0300

    first commit

---
+++ a_main.py
@@ -0,0 +1,26 @@
+
+print('Hello Worl')
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import sys
+
+class MyApp(Gtk.Application):
+
+    def do_activate(self):
+        ''''''
+        #create a gtk window belonging to the application itself
+        janela = Gtk.Window(application=self)
+
+        # set the title
+        janela.set_title('Welcome to Gnome')
+
+        #show the window
+        janela.show_all()
+
+
+#create and the run application, exit the value
+app = MyApp()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
