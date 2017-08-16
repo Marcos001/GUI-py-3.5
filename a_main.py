e22ce42b91b7df4d07af3f7d68f5a54749d9caaa
Marcos001 <santosMsantos01@gmail.com>
2017-08-16 12:46:56 -0300

    segundo commit

--- a_main.py
+++ a_main.py
@@ -16,6 +16,9 @@ class MyApp(Gtk.Application):
         # set the title
         janela.set_title('Welcome to Gnome')
 
+        # set default size of the window
+        janela.set_default_size(300,200)
+
         #show the window
         janela.show_all()
 
