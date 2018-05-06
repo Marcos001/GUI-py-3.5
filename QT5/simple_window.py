60373c4ddb86178ec46be3a78b7fdb894adc26a7
Nigon001 <ferreiraviniciussdi@gmail.com>
2018-05-06 20:12:30 -0300

    trabalhando com qt5

--- QT5/simple_window.py
+++ QT5/simple_window.py
@@ -1,5 +1,30 @@
 
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
 class window(QWidget):
+
+    #constutor
+    def __init__(self, parent=None):
+
+        super(window, self).__init__(parent)
+        self.setWindowTitle("Hello Word")
+        self.resize(450,300)
+
+        #icon of application
+        self.setWindowIcon(QIcon('../asserts/icon_unix.png'))
+
+
+
+if __name__ == '__main__':
+
+    # create PyQt5 application project
+    app = QApplication([])
+
+    janela = window()
+
+    janela.show()
+
+    sys.exit(app.exec_())
+
